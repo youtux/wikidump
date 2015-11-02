56d8534949104979d713f813c93355518b56a7e5
Alessio Bogon <youtux@gmail.com>
2015-11-02 14:38:25 +0100

    implement range inclusion for Span

--- wikidump/extractors/common.py
+++ wikidump/extractors/common.py
@@ -1,5 +1,15 @@
 import collections
 
-Span = collections.namedtuple('Span', 'begin end')
 CaptureResult = collections.namedtuple('CaptureResult', 'data span')
 Identifier = collections.namedtuple("Identifier", ['type', 'id'])
+
+
+class Span(collections.namedtuple('Span', 'begin end')):
+
+    def __le__(self, other):
+        # return self.begin >= other.begin and self.end <= other.end
+        # HACK: the following is more efficient. Sorry :(
+        return self[0] >= other[0] and self[1] <= other[1]
+
+    def __lt__(self, other):
+        return self[0] > other[0] and self[1] < other[1]
