a35d36884ce9ebc2c64b5919f39fad5cf9ffaa1c
Alessio Bogon <youtux@gmail.com>
2015-11-03 15:51:16 +0100

    some tests for templates

--- tests/extractors/test_misc.py
+++ tests/extractors/test_misc.py
@@ -1,51 +1,102 @@
-from wikidump.extractors.misc import Section, sections
+from wikidump.extractors.misc import Section, sections, templates
 
-import collections
+from .utils import assert_captures_in_text
 
-INPUT_TEXT = '''
-Garbage
-
-= Section 1 =
-Lorem ipsum
-lorem ipsummmm
-
-== Section 2 ==
-asdnot
- dsa
- datad
-
- def a():
-     pass
-ddase
-s
-'''
-EXPECTED = collections.Counter([
-    Section(name=' Section 1 ', level=1, body='''\
-Lorem ipsum
-lorem ipsummmm
-'''
-    ),
-    Section(name=' Section 2 ', level=2, body='''\
-asdnot
- dsa
- datad
-
- def a():
-     pass
-ddase
-s
-'''
-    ),
-])
+from textwrap import dedent
 
 
-def test_extract():
-    captures = sections(INPUT_TEXT)
-    found_sections = collections.Counter(capture.data
-        for capture in captures)
+def test_sections():
+    text = dedent('''\
+        Garbage
 
-    assert found_sections == EXPECTED
+        = Section 1 =
+        Lorem ipsum
+        lorem ipsummmm
 
-    for _, (capture_begin, capture_end) in captures:
-        assert 0 <= capture_begin <= len(INPUT_TEXT)
-        assert 0 <= capture_end <= len(INPUT_TEXT)
+        == Section 2 ==
+        asdnot
+         dsa
+         datad
+        ''')
+
+    expected = [
+        Section(name=' Section 1 ', level=1, body=dedent('''\
+            Lorem ipsum
+            lorem ipsummmm
+            ''')
+        ),
+        Section(name=' Section 2 ', level=2, body=dedent('''\
+            asdnot
+             dsa
+             datad
+            ''')
+        ),
+    ]
+
+    captures = list(sections(text))
+    found_sections = [capture.data for capture in captures]
+
+    assert found_sections == expected
+
+    assert_captures_in_text(captures, text)
+
+
+def test_oneline_template():
+    text = dedent('''\
+        {{cite journal |vauthors = Poland GA, Jacobson RM | title = The Age-Old Struggle against the Antivaccinationists | journal = N Engl J Med | volume = 364 | pages = 97–9 | date = 13 January 2011 | pmid = 21226573 | doi = 10.1056/NEJMp1010594 | url = http://www.nejm.org/doi/full/10.1056/NEJMp1010594 | archiveurl = http://web.archive.org/web/20140423082318/http://www.nejm.org/doi/full/10.1056/NEJMp1010594 | archivedate = 23 April 2014 }}''')
+    captures = list(templates(text))
+
+    found_templates = [capture.data for capture in captures]
+
+    assert found_templates == [text]
+
+    assert_captures_in_text(captures, text)
+
+
+def test_multiline_template():
+    text = dedent('''\
+        {{cite journal
+        | vauthors = Poland GA, Jacobson RM
+        | title = The Age-Old Struggle against the Antivaccinationists
+        | journal = N Engl J Med
+        | volume = 364
+        | pages = 97–9
+        | date = 13 January 2011
+        | pmid = 21226573
+        | doi = 10.1056/NEJMp1010594
+        | url = http://www.nejm.org/doi/full/10.1056/NEJMp1010594
+        | archiveurl = http://web.archive.org/web/20140423082318/http://www.nejm.org/doi/full/10.1056/NEJMp1010594
+        | archivedate = 23 April 2014 }}''')
+    captures = list(templates(text))
+
+    found_templates = [capture.data for capture in captures]
+
+    assert found_templates == [text]
+
+    assert_captures_in_text(captures, text)
+
+
+def test_fucked_up_template():
+    # As bad as it could be
+    text = dedent('''\
+        {{
+
+
+                        cite journal
+        |
+        vauthors = Poland GA, Jacobson RM |
+        title = The Age-Old Struggle
+
+
+        against the Antivaccinationists | journal
+        = N Engl J Med | volume = 364 | pages = 97–9 | date = 13
+        January 2011 | pmid = 21226573 | doi = 10.1056/NEJMp1010594 | url = http://www.nejm.org/doi/full/10.1056/NEJMp1010594 |
+                                     archiveurl = http://web.archive.org/web/20140423082318/http://www.nejm.org/doi/full/10.1056/NEJMp1010594 | archivedate = 23 April 2014 }}''')
+
+    captures = list(templates(text))
+
+    found_templates = [capture.data for capture in captures]
+
+    assert found_templates == [text]
+
+    assert_captures_in_text(captures, text)
